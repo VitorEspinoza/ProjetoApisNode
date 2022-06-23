206c51d764db1f41e96973514ce5f27b4fa3b2f9
Espinoza <vitorespinoza18@gmail.com>
2022-06-23 01:09:26 -0300

    Criando model de usuario

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        _id: {type: Number},
+        name: {type: String, required: true},
+        cpf: {type: String, required: true},
+        bithDate: {type: Date}, required: true,
+        email: {type: String, required: true},
+        address: {type: String, required: true},
+        number: {type: String, required: true},
+        complement: {type: String, required: true},
+        city: {type: String, required: true},
+        state: {type: String, required: true},
+        country: {type: String, required: true},
+        zipCode: {type: String, required: true}
+    }
+);
+
+const users = mongoose.model('users', userSchema);
+
+export default users;
