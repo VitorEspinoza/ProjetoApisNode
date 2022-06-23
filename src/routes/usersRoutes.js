85b438f8d78de25b5337e3526f2f8ef3de588be2
Espinoza <vitorespinoza18@gmail.com>
2022-06-23 02:09:30 -0300

    Corrigindo erro de roteamento

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -4,9 +4,9 @@ import UserController from "../controllers/usersController.js";
 const router = express.Router();
 
 router
-    .get("api/v1/users", UserController.getUsers)
-    .get("api/v1/users/:id", UserController.getUsersById)
-    .post("api/v1/users", UserController.addUser)
-    .put("api/v1/users/:id", UserController.updateUser)
-    .delete("api/v1/users/:id", UserController.deleteUser)
+    .get("/api/v1/users", UserController.getUsers)
+    .get("/api/v1/users/:id", UserController.getUsersById)
+    .post("/api/v1/users", UserController.addUser)
+    .put("/api/v1/users/:id", UserController.updateUser)
+    .delete("/api/v1/users/:id", UserController.deleteUser)
     export default router;
